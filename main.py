0f857a8c71d9435db2276d489f49390a1f68c1c2
liasd <lias.desmedt@ugent.be>
2023-10-24 11:58:12 +0200

    functions combined

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def print_all():
     print("t_strings:", t_strings)
     print("R_subsets:", R_subsets)
 
-print_all()
+#print_all()
 
 
 
@@ -70,3 +70,51 @@ print(len(s))
 k,s,t_strings,R_subsets = alphabet_prune(k,s,t_strings,R_subsets)
 print_all()
 print("Pruning done..")
+
+def TestIfSubstring(s,new_t_i):
+    compiled_regex =re.compile(new_t_i)
+    return compiled_regex.search(s) is not None
+
+def findSolutions(s,t,i,R,Gamma,subsitutions):
+    # if no gamma's are left a solution has been found
+    if i>= len(R.keys()):
+        return subsitutions
+    
+    gam= Gamma[i]
+    new_t=[]
+    # try all r
+    for j in range(len(R[gam])):
+        succes=True
+        #apply subsitution for each t
+        for t_i in t:
+            new_t_i=""
+            test_t_i=r""
+            for c in t_i:
+                if c== gam:
+                    new_t_i+= R[gam][j]
+                    test_t_i+= R[gam][j]
+                elif c in Gamma:
+                    test_t_i+= "."
+                    new_t_i+= c
+                else:
+                    new_t_i+= c
+                    test_t_i+= c
+            new_t.append(new_t_i)
+            #test new_t_i
+            isSubstring= TestIfSubstring(s,test_t_i)
+            if not isSubstring:
+                succes=False
+                break
+        if succes:
+            subsitutions[i]=R[gam][j]
+            result= findSolutions(s,new_t,i+1,R,Gamma,subsitutions)
+            if result:
+                return result
+    return False
+
+result=findSolutions(s,t_strings,0,R_subsets,list(R_subsets.keys()),{})
+print(result)
+    
+        
+
+
